750378dfa4330356ce94fe2aedffafb83d34456f
PaperOrb <48017994+PaperOrb@users.noreply.github.com>
2021-04-19 18:00:28 -0400

    feat: create html, css, and add nav js logic skeleton

---
+++ index.js
@@ -0,0 +1,51 @@
+const nav = document.querySelector(".nav-list");
+const newsBtn = document.querySelector(".news");
+const photosBtn = document.querySelector(".photos");
+const moreBtn = document.querySelector(".more");
+const dropdown = document.querySelector(".dropdown-container");
+let tabId = "news";
+
+const carousel = (function () {
+  const carousel = document.querySelector(".carousel");
+  const carouselTrack = document.querySelector(".carousel-track");
+
+  function gotoLeftImg() {
+    alert("yep");
+  }
+
+  function gotoRightImg() {
+    alert("yep");
+  }
+
+  function gotoClickedImg() {}
+
+  function timer() {}
+
+  function display(property) {
+    carousel.style.display = property;
+  }
+
+  return { display, gotoLeftImg, gotoRightImg, gotoClickedImg, timer };
+})();
+
+const news = (function () {
+  const newsContent = document.querySelector(".news-content");
+
+  function display(property) {
+    newsContent.style.display = property;
+  }
+
+  return { display };
+})();
+
+function renderDropdown(dropdown) {
+  dropdown.toggleAttribute("hidden");
+}
+
+nav.addEventListener("click", (e) => {
+  tabId = e.target.id; // catch edge cases when nav is clicked instead of tab displaying
+
+  tabId === "photos" ? carousel.display("flex") : carousel.display("none");
+  tabId === "news" ? news.display("flex") : news.display("none");
+  if (tabId === "more") renderDropdown(dropdown);
+});
